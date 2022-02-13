408ae6c56b9c850f42ffeea3eee983de06dc8bdb
yashar <yasharsanayee@gmail.com>
2022-02-13 10:51:34 +0330

    product list

--- projects/kilid-plp/src/app/main/main.module.ts
+++ projects/kilid-plp/src/app/main/main.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { MainRoutingModule } from './main-routing.module';
+import { ProductListComponent } from './product-list/product-list.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ProductListComponent
+  ],
   imports: [
     CommonModule,
     MainRoutingModule
