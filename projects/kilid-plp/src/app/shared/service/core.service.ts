d671faaa0b0eb9052d5cc245764e6e14dbf0500f
yashar <yasharsanayee@gmail.com>
2022-02-13 11:21:22 +0330

    core service done

--- projects/kilid-plp/src/app/shared/service/core.service.ts
+++ projects/kilid-plp/src/app/shared/service/core.service.ts
@@ -1,9 +1,27 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpResponse} from '@angular/common/http';
+import {Observable} from 'rxjs';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
-export class CoreService {
+export abstract class CoreService {
 
-  constructor() { }
+  protected constructor(private httpClient: HttpClient) {
+  }
+
+  get<T>(url: string): Observable<HttpResponse<T>> {
+    return this.httpClient.get<T>(url, {
+      observe: 'response',
+      responseType: 'json',
+    });
+  }
+
+  post<T, D>(url: string, body: D): Observable<HttpResponse<T>> {
+    return this.httpClient.post<T>(url, body, {
+      observe: 'response',
+      responseType: 'json',
+    });
+  }
+
 }
