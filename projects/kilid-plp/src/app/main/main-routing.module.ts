0c35a3698a35aa14e16af5d267180aa46d04d68a
yashar <yasharsanayee@gmail.com>
2022-02-13 10:49:39 +0330

    lazy load main module

--- projects/kilid-plp/src/app/main/main-routing.module.ts
+++ projects/kilid-plp/src/app/main/main-routing.module.ts
@@ -1,10 +1,15 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {RouterModule, Routes} from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: ':searchType/:city',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class MainRoutingModule { }
+export class MainRoutingModule {
+}
