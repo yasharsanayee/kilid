5440aa1532ddcb2a88387991a7a87d34aad90f48
yashar <yasharsanayee@gmail.com>
2022-02-13 11:42:00 +0330

    core service refactored, http and http by response

--- projects/kilid-plp/src/app/shared/service/core/core.service.ts
+++ projects/kilid-plp/src/app/shared/service/core/core.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {HttpClient, HttpResponse} from '@angular/common/http';
 import {Observable} from 'rxjs';
+import {map} from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -10,14 +11,28 @@ export abstract class CoreService {
   protected constructor(private httpClient: HttpClient) {
   }
 
-  get<T>(url: string): Observable<HttpResponse<T>> {
+  get<T>(url: string): Observable<T> {
+    return this.httpClient.get<T>(url, {
+      observe: 'response',
+      responseType: 'json',
+    }).pipe(map(value => value.body));
+  }
+
+  getByResponse<T>(url: string): Observable<HttpResponse<T>> {
     return this.httpClient.get<T>(url, {
       observe: 'response',
       responseType: 'json',
     });
   }
 
-  post<T, D>(url: string, body: D): Observable<HttpResponse<T>> {
+  post<T, D>(url: string, body: D): Observable<T> {
+    return this.httpClient.post<T>(url, body, {
+      observe: 'response',
+      responseType: 'json',
+    }).pipe(map(value => value.body));
+  }
+
+  postByResponse<T, D>(url: string, body: D): Observable<HttpResponse<T>> {
     return this.httpClient.post<T>(url, body, {
       observe: 'response',
       responseType: 'json',
